68e2a3527e2b59fb5e4bcbe983f83abaafca6c2a
nodadyoushutup <admin@nodadyoushutup.com>
2024-07-06 20:02:16 +0000

    [WIP] Docker

--- script/radarr.sh
+++ script/radarr.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 # script/radarr.sh
 
-# Use absolute paths for sourcing
 source /script/utils.sh
 
-# Debug: List files in /script directory
-ls -la /script
-
-# Run the necessary functions
 namespace-check
 pvc-bound
 set-arr-config
-
-# Keep the container running for debugging purposes
-sleep 10000000
--- script/sonarr.sh
+++ script/sonarr.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# script/sonarr.sh
 
 source /script/utils.sh
 
